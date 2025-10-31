49d11d5c95990091c1bc1fbd9b97bf3d07d8c9c3
Aymeric DJERIDI <adjeridi@ADEI.local>
2025-10-31 10:29:09 +0100

    retour au bracket après déconnexion

--- src/Bracket.js
+++ src/Bracket.js
@@ -167,16 +167,19 @@ export default function Bracket({ user }) {
 
   return (
     <>
-      <div style={{
-        background: user ? (canEdit ? "#d4edda" : "#fff3cd") : "#f8d7da",
-        color: "#333",
-        padding: "10px",
-        borderRadius: "8px",
-        margin: "10px",
-        textAlign: "center",
-      }}>
-        {user ? canEdit ? "✅ Connecté et autorisé à modifier" : "⚠️ Connecté, lecture seule" : "🔒 Non connecté — lecture seule"}
-      </div>
+        <div style={{
+            background: user ? (canEdit ? "#d4edda" : "#fff3cd") : "#f8d7da",
+            color: "#333",
+            padding: "10px",
+            borderRadius: "8px",
+            margin: "10px",
+            textAlign: "center",
+        }}>
+          {user ? canEdit ? "✅ Connecté et autorisé à modifier"
+                        : "⚠️ Connecté, lecture seule"
+              : "🔒 Non connecté — lecture seule"}
+        </div>
+
 
       {/* CONTROLS */}
       <div className="controls">
